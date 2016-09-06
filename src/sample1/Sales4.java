5333f71b20b301729b0dc4e768464450bf8d30ea
柿添　航 <kakizoe@tagbangers.co.jp>
2016-09-06 13:47:25 +0900

    サンプル４終わりました。

---
+++ src/sample1/Sales4.java
@@ -0,0 +1,44 @@
+package sample1;
+
+/**
+ * Created by kakizoewataru on 2016/09/06.
+ */
+public class Sales4 {
+    public static int total;
+
+    public static void main(String[] args){
+
+        int [] records;
+        records = new int [4];
+        records[0] = 0;
+        records[1] = 65000;
+        records[2] = 73000;
+        records[3] = 90000;
+
+        String date = "** 2010/04/01 **";
+
+        for(int i = 0 ; i <= 2 ; i++){
+            if(i == 0){
+                System.out.println("[0] total => " +records[0]);
+            }
+            if(i == 1){
+                System.out.println("[1] total => " +records[1]);
+            }
+            if(i == 2){
+                System.out.println("[2] total => " +(records[1]+records[2]));
+            }
+        }
+
+        total = records[1] + records[2] + records[3];
+        System.out.println("" +date);
+        System.out.println("合計 : " +total);
+
+        getAverage ();
+    }
+
+    public static void getAverage(){
+        int average;
+        average = total / 3;
+        System.out.println("平均 : " +average);
+    }
+}
